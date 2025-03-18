6b7bfe0e44f42ec12fabdecc13fa962e5fc38547
Manav-web <bajrangbali557@gmail.com>
2025-03-18 14:19:06 +0530

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "MUSKAN"],
+    strings: ["HAPPY", "BIRTHDAY!", "TO YOU"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
